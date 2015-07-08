7688a91df42cec94b0b10523469a0283510eadf8
Arnaud Tarroux <ta.arnaud@gmail.com>
2015-07-08 23:59:35 +0200

    New landing page

--- _src/js/main.js
+++ _src/js/main.js
@@ -17,15 +17,15 @@ $(document).ready( function() {
 	$('.header-arrow').click( function(e) {
 		e.preventDefault();
 		$('body').animate({
-			scrollTop: $('.newsletter').offset().top
+			scrollTop: $('.inter-section').offset().top
 		});
 	});
 
-	$('a[href^="#"]').click(function(){  
-    		var id = $(this).attr("href");
-    		var offset = $(id).offset().top 
-    		$('html, body').animate({scrollTop: offset}, 'slow'); 
-    		return false;  
+	$('#header .button').click( function(e) {
+		e.preventDefault();
+		$('body').animate({
+			scrollTop: $('#newsletter').offset().top
+		});
 	});
 
 });
